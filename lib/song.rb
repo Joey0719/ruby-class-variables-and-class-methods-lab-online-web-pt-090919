d7847eb0d4f4464d1ce631eafee09f637be29151
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-19 19:20:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Song 
+
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre 
+    @@count += 1 
+    @@artists << artist 
+    @@genres
