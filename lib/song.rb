12443523f2acde9270ae2ac6dc861f74350f9d25
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-19 19:27:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,3 +23,13 @@ class Song
   def self.artists
     @@artists.uniq 
   end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
